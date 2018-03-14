c4a920865b120f417131b8a72464d18284aa763d
05050775 <37134199+05050775@users.noreply.github.com>
2018-03-14 15:46:15 +0800

    Add files via upload

---
+++ HW2.ino
@@ -0,0 +1,34 @@
+const byte startPin =15;
+const byte endPin = 8;
+
+void setup()
+{
+   for (byte i = startPin; i>= endPin; i--) 
+    {
+    pinMode(i, OUTPUT);
+  }
+}
+void loop()
+{
+  byte i;
+  
+    for (i=startPin;i>=endPin;i--) 
+    {
+    digitalWrite(i,HIGH);
+  }
+  
+  for(i=startPin;i>=endPin;i--)
+    {  
+      digitalWrite(i,LOW);
+    delay(100);
+      digitalWrite(i,HIGH);
+      delay(100);
+    }
+    for(i=endPin;i<=startPin;i++)
+    {  
+      digitalWrite(i,LOW);
+    delay(100);
+      digitalWrite(i,HIGH);
+      delay(100);
+    } 
+} 
